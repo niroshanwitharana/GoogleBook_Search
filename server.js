b1f19d6281a396c455f80585a7edb9dd76f986e3
niroshanwitharana <nrosh_uoc@yahoo.com>
2020-11-16 00:12:39 +0000

    DB changed to Atlas

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require ("mongoose");
 const routes= require ("./routes");
+require("dotenv").config();
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
